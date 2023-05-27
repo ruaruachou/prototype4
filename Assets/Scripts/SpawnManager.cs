cb9a2fc3eeb90fe1fb2490182b93f8d93cfa9fda
MyName <yyyplaywf@163.com>
2023-05-27 11:49:24 +0800

    5-27-1

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,26 +1,43 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[]enemyLst;
+    //敌人数组：用于存放各种敌人
+    public GameObject[] enemyLst;
+    //波次数组：数组长度为波次数量，每个元素的值为本波生成的数量
+    public int[] wave;
+
+    public int enemyCount;//显示当前实时敌人数量
+
     void Start()
     {
-        InvokeRepeating("Spawn", 2, 5);
+
     }
 
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        for (int i = 0; i < wave.Length; )
+        {
+                for (int j = 0; j < 5; j++)
+                {
+                    Spawn();
+                if (enemyCount == 0)
+                    i++;
+                }
+        }
     }
 
     void Spawn()
     {
         int enemyIndex = Random.Range(0, enemyLst.Length - 1);
+
         Instantiate(enemyLst[enemyIndex], Random.insideUnitSphere * 3, transform.rotation);
     }
-
-    //public Vector3 RandomPos()
-
 }
+
+
