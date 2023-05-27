f4aeee1d625950880fefe3e9e499c4f2ec7e6347
John Doe <johndoe@example.com>
2023-05-27 12:28:48 +0800

    2-27-2

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -10,31 +10,34 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] enemyLst;
     //波次数组：数组长度为波次数量，每个元素的值为本波生成的数量
     public int[] wave;
-
     public int enemyCount;//显示当前实时敌人数量
 
     void Start()
     {
-
     }
 
     void Update()
     {
         enemyCount = FindObjectsOfType<Enemy>().Length;
-        for (int i = 0; i < wave.Length; )
+        //i为波次
+        for (int i = 0; i < wave.Length;)
         {
-                for (int j = 0; j < 5; j++)
-                {
-                    Spawn();
+            Debug.Log("i"+i);
+            //j为当前波次的敌人数量
+            for (int j = 0; j < wave[i]; j++)
+            {
+                Debug.Log("j"+j);
+                Spawn();
                 if (enemyCount == 0)
                     i++;
-                }
+            }
         }
     }
 
+    //生成敌人
     void Spawn()
     {
-        int enemyIndex = Random.Range(0, enemyLst.Length - 1);
+        int enemyIndex = Random.Range(0, enemyLst.Length );
 
         Instantiate(enemyLst[enemyIndex], Random.insideUnitSphere * 3, transform.rotation);
     }
